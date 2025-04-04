55460a690340a918e1b2010fa1c4436020240dc8
23103Karamu <23103@karamu.nz>
2025-04-04 13:10:01 +1300

    Prevented people from inputting incorrect items

--- test_assessment.py
+++ test_assessment.py
@@ -13,7 +13,8 @@ while Student_age is None: ## If the student doesn't input an integer for their 
         Student_age = int(input("Enter an age: "))
     except ValueError:
         print("Please input an integer")
-    except Student_age <= 0 or Student_age >= 100:
+        continue
+    if Student_age <= 0 or Student_age >= 100:
         print("Are you sure that is your age?")
         Student_age = None
     else:
@@ -21,8 +22,10 @@ while Student_age is None: ## If the student doesn't input an integer for their 
 
 ## Checking if the student is eligible for the Club Day
 Student_eligibility = False
-if Student_age <= 11 or Student_age >= 18:
-    print("Sorry but you are not eligible for the Club Day")
+if Student_age <= 11:
+    print("Sorry, but you are not eligible for the Club Day due to being too young")
+elif Student_age >= 18:
+    print("Pedophiles are not allowed")
 else:
     Student_eligibility = True
 
@@ -30,8 +33,12 @@ else:
 Cost = 0
 Are_going = None
 Chosen_activity = None
-while Chosen_activity == None and Student_eligibility == True: ## Using a while Chosen_activity = "": so if they put a number that is not availible it'll loop back
-    Chosen_activity = int(input("\nChoose an activity:\n1. Music Jam Session (2 hours, easy, $5 fee)\n2. Science Experiments Lab (3 hours, moderate, $10 fee)\n3. Sports Leadership Training (4 hours, challenging, $12 fee)\nEnter the number of your chosen activity: "))
+while Chosen_activity == None and Student_eligibility == True: ## Using a while Chosen_activity = None: so if they put a number that is not availible it'll loop back
+    try:
+        Chosen_activity = int(input("\nChoose an activity:\n1. Music Jam Session (2 hours, easy, $5 fee)\n2. Science Experiments Lab (3 hours, moderate, $10 fee)\n3. Sports Leadership Training (4 hours, challenging, $12 fee)\nEnter the number of your chosen activity: "))
+    except ValueError:
+       print("Please input the number of your decision")
+       continue
     if Chosen_activity == 1:
         Chosen_activity = "Music Jam Session (2 hours, easy)"
         Cost = Cost + 5
@@ -44,12 +51,17 @@ while Chosen_activity == None and Student_eligibility == True: ## Using a while 
         Chosen_activity = "Sports Leadership Training (4 hours, challenging)"
         Cost = Cost + 12
         break
-    elif Chosen_activity != 1 or 2 or 3:
+    else:
         print("Invalid choice, try again.")
         Chosen_activity = None
+        continue
 Chosen_meal = None
 while Chosen_meal == None and Student_eligibility == True: ## If an invalid choice for meal is chosen it will loop back
-    Chosen_meal = int(input("\nMeal options:\n1. Standard\n2. Vegetarian\n3. Dairy-free\n4. No meal\nEnter the number of your meal choice: "))
+    try:
+        Chosen_meal = int(input("\nMeal options:\n1. Standard\n2. Vegetarian\n3. Dairy-free\n4. No meal\nEnter the number of your meal choice: "))
+    except ValueError:
+        print("Please input the number of your decision")
+        continue
     if Chosen_meal == 1:
         Chosen_meal = "Standard"
         Cost = Cost + 7
@@ -66,15 +78,16 @@ while Chosen_meal == None and Student_eligibility == True: ## If an invalid choi
     else:
         print("Invalid choice, try again.")
         Chosen_meal = None
+        continue
 
-    while Are_going == None and Student_eligibility == True:
-        Are_going = input(f'\n{Student_name}, age {Student_age}, activity chosen: {Chosen_activity}, meal option: {Chosen_meal}, cost ${Cost}. Are you attending (yes/no)? ').lower()
-        if Are_going == "yes" or Are_going == "y":
-            print(f"\n{Student_name} is confirmed for {Chosen_activity}. See you there!")
-            break
-        elif Are_going == "no" or Are_going == "n":
-            print(f"\n{Student_name} will not be attending tonight.")
-            break
-        else:
-            print("Invalid choice, try again.")
-            Are_going = ""
+while Are_going == None and Student_eligibility == True:
+    Are_going = input(f'\n{Student_name}, age {Student_age}, activity chosen: {Chosen_activity}, meal option: {Chosen_meal}, cost ${Cost}. Are you attending (yes/no)? ').lower()
+    if Are_going == "yes" or Are_going == "y":
+        print(f"\n{Student_name} is confirmed for {Chosen_activity}. See you there!")
+        break
+    elif Are_going == "no" or Are_going == "n":
+        print(f"\n{Student_name} will not be attending tonight.")
+        break
+    else:
+        print("Invalid choice, try again.")
+        Are_going = None
